2bbd013c1a23722ad1ace1551c13f1ceaca9ab91
headbwoi <tochukwujohn24@gmail.com>
2022-07-12 12:22:12 +0100

    added mobile menu toggle

---
+++ index.js
@@ -0,0 +1,13 @@
+const body = document.body
+const mobileMenu = document.querySelector(".mobilemenu")
+const hamburger = document.querySelector(".hamburger")
+const close = document.querySelector(".close")
+
+hamburger.addEventListener("click", () => {
+    mobileMenu.classList.add("active")
+    body.classList.add("overlay")
+})
+close.addEventListener("click", () => {
+    mobileMenu.classList.remove("active")
+    body.classList.remove("overlay")
+})
