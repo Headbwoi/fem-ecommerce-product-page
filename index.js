54b9a5f5077b22bf0d52be3d9dfba927cc7aef3b
headbwoi <tochukwujohn24@gmail.com>
2022-07-17 11:56:34 +0100

    finished sliders

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ close.addEventListener("click", () => {
   mobileMenu.classList.remove("active")
 })
 
-
 //slider
 const mobileSliderRight = document.querySelector("[data-sliderMobileRight]")
 const mobileSliderLeft = document.querySelector("[data-sliderMobileLeft]")
 const imagesSlideContainer = document.querySelector("[data-imagesSlide]")
-const imagesSlide = [...imagesSlideContainer.querySelectorAll("img")]
+const imagesSlide = imagesSlideContainer.querySelectorAll("img")
 
 imagesSlideContainer.style.transform = "translateX(0px)"
 
@@ -51,7 +50,6 @@ mobileSliderLeft.addEventListener("click", () => slideImageMobile("left"))
 const thumbnailContainer = document.querySelector("[data-thumbnails]")
 const thumbnails = [...thumbnailContainer.querySelectorAll(".thumbnail")]
 
-let slideIndex = 0
 //image thumnbails desktop//
 const removeThumbnailActiveClass = () => {
   thumbnails.forEach(thumbnail => {
@@ -62,7 +60,7 @@ thumbnails.forEach((thumbnail, index) => {
   thumbnail.addEventListener("click", () => {
     removeThumbnailActiveClass()
     thumbnail.classList.add("active")
-    changeSlide(slideIndex = index)
+    changeSlide(index)
   })
 })
 
@@ -70,7 +68,7 @@ function changeSlide(n){
   imagesSlide.forEach(img => {
     img.style.display = "none"
   })
-  imagesSlide[slideIndex].style.display = "flex"
+  imagesSlide[n].style.display = "flex"
 }
 
 //modal elements//
@@ -79,7 +77,7 @@ const modalIconClose = document.querySelector("[data-Modal-Icon]")
 const modalIconRight = document.querySelector("[data-modal-right]")
 const modalIconLeft = document.querySelector("[data-modal-left]")
 const modalSliderContainer = document.querySelector("[data-sliderModal]")
-const modalSlides = [...modalSliderContainer.querySelectorAll("img")]
+const modalSlideImage = modalSliderContainer.querySelector("img")
 const modalThumbnailContainer = document.querySelector("[data-modal-thumbnail]")
 const modalThumbnails = [...modalThumbnailContainer.querySelectorAll(".thumbnail")]
 
@@ -97,8 +95,11 @@ function openModal(index) {
   if (window.screen.width > 1110) {
     modal.classList.add("fade-in")
     modal.classList.remove("fade-out")
+    modalSlideImage.src=`images/image-product-${index + 1}.jpg`
   }
 }
+activeModalIndex = 0
+
 function closeModal() {
     modal.classList.remove("fade-in")
     modal.classList.add("fade-out")
@@ -106,28 +107,18 @@ function closeModal() {
 
 modalIconClose.addEventListener("click", closeModal)
 //modal slide
-activeModalIndex = 0
+let slideIndex = 1
 const slideImageModal = (direction) => {
-  let width = modalSliderContainer.clientWidth
-  let imgLength = modalSlides.length - 1
-  if (direction === "right") {
-    activeModalIndex++
-    if (activeModalIndex > imgLength) {
-      activeModalIndex = 0
-    }
-    modalSliderContainer.style.transform = `translateX(-${
-      activeModalIndex * width
-    }px)`
-  } else if (direction === "left") {
-    activeModalIndex--
-    if (activeModalIndex < 0) {
-      activeModalIndex = imgLength
-    }
-    modalSliderContainer.style.transform = `translateX(-${
-      activeModalIndex * width
-    }px)`
+  if(direction === "right"){
+    slideIndex++
+    if(slideIndex > 4){slideIndex = 1}
+  }else if(direction === "left"){
+    slideIndex--
+    if(slideIndex < 1){slideIndex = 4}
   }
+  modalSlideImage.src=`images/image-product-${slideIndex}.jpg`
 }
+
 modalIconRight.addEventListener("click", () => slideImageModal("right"))
 modalIconLeft.addEventListener("click", () => slideImageModal("left"))
 
@@ -141,25 +132,56 @@ modalThumbnails.forEach((thumb, index) => {
   thumb.addEventListener("click", () => {
     removeModalThumbnailActiveClass()
     thumb.classList.add("active")
-    changeModal(slideModal = index)
+    changeModal(index + 1)
   })
 })
-let slideModal = 0
 function changeModal(n){
-  let width = modalSliderContainer.clientWidth
-  let imgLength = modalSlides.length - 1
-  if(n > imgLength){n = 0}
-  if(n < 0){n = imgLength}
-  modalSlides[n].style.transform = `translateX(-${
-      n * width
-    }px)`
+  modalSlideImage.src=`images/image-product-${n}.jpg`
 }
 
 //cart elements//
 const cartToggle = document.querySelector("[data-cartToggle]")
 const cartCard = document.querySelector("[data-cartCard]")
+const numberInCartEl = document.querySelector("[data-cart-count]")
+const cartCardDeleteEl = document.querySelector("[data-item-delete]")
+const cartCardItemEl = document.querySelector("[data-cart-item]")
+const cartItemPrice = document.querySelector("#price")
+const cartItemCount = document.querySelector(".count")
+const cartItemAmount = document.querySelector(".amount")
+const productCountMinusEl = document.querySelector("[data-count-minus]")
+const productCountPlusEl = document.querySelector("[data-count-plus]")
+const productCountEl = document.querySelector("[data-product-count]")
+const emptyCartTextEl = document.querySelector(".empty")
+const addToCartEl = document.querySelector("[data-add-to-cart]")
+const itemCheckoutEl = document.querySelector("[data-item-checkout]")
 
 //cart  card toggle
 cartToggle.addEventListener("click", () => {
   cartCard.classList.toggle("hidden")
+})
+
+let productCount = 0
+
+productCountMinusEl.addEventListener("click", () => {
+  if(productCount < 0)return
+  productCountEl.innerText = productCount--
+})
+productCountPlusEl.addEventListener("click", () => {
+  productCountEl.innerText = productCount++
+})
+
+numberInCartEl.innerText = productCount
+
+const calculatePrice = () => {
+  cartItemCount.innerText = productCount
+  let calc = Number(cartItemCount.innerText) * 125
+  cartItemAmount.innerText = `$${calc}.00`
+}
+
+addToCartEl.addEventListener("click", () => {
+  if (productCount == 0) {
+    emptyCartTextEl.classList.remove("hidden")
+
+  }
+  calculatePrice()
 })
